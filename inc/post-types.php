d7a3e2ba79b43cb9c128da6a9c0e1b0ddbb5d305
Austin Crane <austincrane@gmail.com>
2018-08-21 11:47:36 -0400

    add yoga

--- inc/post-types.php
+++ inc/post-types.php
@@ -101,6 +101,39 @@ function js_custom_init()
   
   ); 
   register_post_type('demo_clinic',$args); // name used in query
+
+
+
+   $labels = array(
+  'name' => _x('Yoga', 'post type general name'),
+    'singular_name' => _x('Yoga', 'post type singular name'),
+    'add_new' => _x('Add New', 'Yoga'),
+    'add_new_item' => __('Add New Yoga'),
+    'edit_item' => __('Edit Yoga'),
+    'new_item' => __('New Yoga'),
+    'view_item' => __('View Yoga'),
+    'search_items' => __('Search Yoga'),
+    'not_found' =>  __('No Yoga found'),
+    'not_found_in_trash' => __('No Yoga found in Trash'), 
+    'parent_item_colon' => '',
+    'menu_name' => 'Yoga'
+  );
+  $args = array(
+  'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+    'menu_position' => 20,
+    'supports' => array('title','editor','custom-fields','thumbnail'),
+  
+  ); 
+  register_post_type('yoga',$args); // name used in query
   
   // Add more between here
   
