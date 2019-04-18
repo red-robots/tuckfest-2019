355ee6253121bf4a02e4d00b8b6996e37fda3b78
Austin Crane <austincrane@gmail.com>
2019-04-18 12:31:40 -0400

    update mobile nav

--- header.php
+++ header.php
@@ -158,7 +158,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	    			<a href="<?php bloginfo('url'); ?>/competition-type/adventure-obstacle/">Adventure & Obstacle</a>
 	    		</li>
 	    		<li>
+	    			<a href="<?php bloginfo('url'); ?>/competition-type/sup/">Sup</a>
+	    		</li>
+	    		<li>
 	    			<a href="<?php bloginfo('url'); ?>/competition-type/bike/">Bike</a>
+	    		</li>
+	    		<li>
+	    			<a href="<?php bloginfo('url'); ?>/competition-type/swim/">Swim</a>
 	    		</li>
 	    		<li>
 	    			<a href="<?php bloginfo('url'); ?>/competition-type/climb/">Climb</a>
@@ -167,25 +173,22 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	    			<a href="<?php bloginfo('url'); ?>/competition-type/deep-water-solo/">Deep Water Solo</a>
 	    		</li>
 	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/competition-type/kayak/">Kayak</a>
+	    			<a href="<?php bloginfo('url'); ?>/competition-type/trail-run/">Trail Run</a>
 	    		</li>
 	    		<li>
 	    			<a href="<?php bloginfo('url'); ?>/competition-type/raft/">Raft</a>
 	    		</li>
 	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/competition-type/sup/">Sup</a>
+	    			<a href="<?php bloginfo('url'); ?>/competition-type/kayak/">Kayak</a>
 	    		</li>
-	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/competition-type/swim/">Swim</a>
-	    		</li>
-	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/competition-type/trail-run/">Trail Run</a>
-	    		</li>
-	    		
 	    	</ul>
     	</li>
-    	<li>
-    		<a href="<?php bloginfo('url'); ?>/demo-clinic-type/demo">Demos & Clinics</a>
+    	<li>Clinics & Films
+    		<ul class="dropdown">
+    			<li><a href="<?php bloginfo('url'); ?>/demo-clinic-type/clinics">Clinics</a></li>
+    			<li><a href="<?php bloginfo('url'); ?>/demo-clinic-type/films">Films</a></li>
+    		</ul>
+    		
 	    </li>
     	<li>
     		<a href="<?php bloginfo('url'); ?>/tuckfest-yoga">Yoga</a>
@@ -196,7 +199,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	    			<a href="<?php bloginfo('url'); ?>/buy/info/">Info</a>
 	    		</li> -->
 	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/buy/registration/">Registration</a>
+	    			<a href="<?php bloginfo('url'); ?>/buy/registration/">Race & Comp Registration</a>
 	    		</li>
 	    		<li>
 	    			<a href="<?php bloginfo('url'); ?>/buy/terrace-access/">Terrace Access</a>
@@ -205,9 +208,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     	</li>
     	<li>About
 	    	<ul class="dropdown">
-	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/about/info/">Info</a>
-	    		</li>
+	    		
 	    		<li>
 	    			<a href="<?php bloginfo('url'); ?>/about/faqs/">FAQ's</a>
 	    		</li>
@@ -218,10 +219,16 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	    			<a href="<?php bloginfo('url'); ?>/about/map/">Map</a>
 	    		</li>
 	    		<li>
-	    			<a target="_blank" href="https://fs24.formsite.com/usnwc/form141/index.html">Vendor Application</a>
+	    			<a href="<?php bloginfo('url'); ?>/about/gallery/">Gallery</a>
 	    		</li>
 	    		<li>
-	    			<a href="<?php bloginfo('url'); ?>/about/gallery/">Gallery</a>
+	    			<a href="<?php bloginfo('url'); ?>/about/accommodations/">Accommodations</a>
+	    		</li>
+	    		<li>
+	    			<a href="<?php bloginfo('url'); ?>/about/food-and-beverage/">Food & Beverage</a>
+	    		</li>
+	    		<li>
+	    			<a href="<?php bloginfo('url'); ?>/about/vendors/">Vendors</a>
 	    		</li>
 	    	</ul>
     	</li>
